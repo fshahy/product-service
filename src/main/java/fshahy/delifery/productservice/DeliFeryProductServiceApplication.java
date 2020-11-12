164a603430609e1c979a47a08fde94138c002535
Farid Shahy <fshahy@gmail.com>
2020-11-12 18:28:40 +0330

    init maven project.

---
+++ src/main/java/fshahy/delifery/productservice/DeliFeryProductServiceApplication.java
@@ -0,0 +1,13 @@
+package fshahy.delifery.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliFeryProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliFeryProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fshahy/delifery/productservice/DeliFeryProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fshahy.delifery.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliFeryProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
